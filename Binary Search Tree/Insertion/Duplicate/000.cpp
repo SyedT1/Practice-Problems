50967bd679ad5a6d2b29197dd811d1fd31d7caf8
Syed Tahsin <syedmuhaimintahsin@gmail.com>
2021-04-19 15:35:53 +0600

    Duplicate elements insertion

---
+++ Binary Search Tree/Insertion/Duplicate/000.cpp
@@ -0,0 +1,56 @@
+#include <iostream>
+using namespace std;
+class tree
+{
+public:
+    int value;
+    tree *left = nullptr, *right = nullptr;
+};
+void insert(tree *&k, int v)
+{
+    if (k == nullptr)
+    {
+        k = new tree{v, nullptr, nullptr};
+        return;
+    }
+    else if (k->value == v)
+    {
+        if (!k->right)
+        {
+            insert(k->right, v);
+        }
+        else
+        {
+            tree *subright = k->right->right, *subleft = k->right->left;
+            tree *temp = new tree{v, subleft, subright};
+            k->right->right = temp;
+            return;
+        }
+    }
+    else if (k->value < v)
+    {
+        insert(k->right, v);
+    }
+    else if (k->value > v)
+    {
+        insert(k->left, v);
+    }
+}
+void inOrder(tree *l)
+{
+    if (!l)
+        return;
+    inOrder(l->left);
+    cout << l->value << ' '; // once the left subtree is fully evaluated
+    inOrder(l->right);
+}
+int main()
+{
+    tree *l = nullptr;
+    int ar[] = {1, 1,2,3,3,3,3,-1, -3, -8, -10,-10,0,0, 0, -2, -7, -6, -5, -4, 10, 10, 10, 10, 10, 9, 19};
+    for (int i : ar)
+    {
+        insert(l, i);
+    }
+    inOrder(l);
+}
