ff541a0a5ed3a76b272e60b555801acbfa00d8d9
Syed Tahsin <syedmuhaimintahsin@gmail.com>
2021-04-21 20:20:08 +0600

    Delete BST keys outside a given range

---
+++ Binary Search Tree/Deletion/001.cpp
@@ -0,0 +1,99 @@
+#include <iostream>
+using namespace std;
+class tree
+{
+public:
+    int value;
+    tree *left = nullptr, *right = nullptr;
+};
+void insert(tree *&k, int v)
+{
+    if (k == nullptr)
+    {
+        k = new tree{v, nullptr, nullptr};
+        return;
+    }
+    else if (k->value < v)
+    {
+        insert(k->right, v);
+    }
+    else if (k->value > v)
+    {
+        insert(k->left, v);
+    }
+}
+
+void deleteNode(tree *&l)
+{
+    tree *sub = l->right;
+    while (sub->left->left != nullptr)
+    {
+        sub = sub->left;
+    }
+    l->value = sub->left->value;
+    tree *temp = sub->left, *rig = sub->left->right;
+    delete temp;
+    sub->left = rig;
+}
+void deleteNode(tree *&l, int from, int to)
+{
+    if (!l)
+        return;
+    deleteNode(l->left, from, to);
+    deleteNode(l->right, from, to);
+    if (!(l->value >= from && l->value <= to))
+    {
+        if (l->right == nullptr && l->left == nullptr)
+        {
+            tree *k = l;
+            delete k;
+            l = nullptr;
+        }
+        else if (l->right == nullptr)
+        {
+            l = l->left;
+        }
+        else if (l->left == nullptr)
+        {
+            l = l->right;
+        }
+        else
+        {
+            if (l->right->left == nullptr)
+            {
+                l = l->right;
+            }
+            else
+            {
+                deleteNode(l);
+            }
+        }
+    }
+}
+void inOrder(tree *l)
+{
+    if (!l)
+        return;
+    inOrder(l->left);
+    cout << l->value << ' ';
+    inOrder(l->right);
+}
+int main()
+{
+    tree *l = nullptr;
+    int ar[] = {-5,3044,55,5,1,2,3,4,7,101,-4455,-564};
+    for (int i : ar)
+    {
+        insert(l, i);
+    }
+    inOrder(l);
+    int from, to;
+    cout << '\n';
+    cout<<"Deletion of keys outside a given range  "<<endl;
+    cout << "Min : ";
+    cin >> from;
+    cout << "Max : ";
+    cin >> to;
+    deleteNode(l, from, to);
+    inOrder(l);
+}
