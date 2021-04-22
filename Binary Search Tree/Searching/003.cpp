e97dc2d78bd0e2bbbcd1fb0a87678f8e463fd591
Syed Tahsin <syedmuhaimintahsin@gmail.com>
2021-04-22 10:32:55 +0600

    kth minimum

---
+++ Binary Search Tree/Searching/003.cpp
@@ -0,0 +1,75 @@
+#include <iostream>
+using namespace std;
+struct tree
+{
+    int value;
+    tree *left = nullptr, *right = nullptr;
+};
+void insert(tree *&k, int v)
+{
+    if (!k)
+    {
+        k = new tree{v, nullptr, nullptr};
+        return;
+    }
+    else if (k->value < v)
+    {
+        insert(k->right, v);
+    }
+    else if (k->value > v)
+    {
+        insert(k->left, v);
+    }
+}
+void minimum(int &m, tree *l, int &r)
+{
+    if (!l)
+        return;
+    minimum(m, l->left, r);
+    --m;
+    if (m == 0)
+    {
+        r = l->value;
+    }
+    minimum(m, l->right, r);
+}
+int minimum(tree *l, int n)
+{
+    int q = 0;
+    minimum(n, l, q);
+    if (!(n < 1))
+    {
+        throw "number of nodes is less than the query";
+    }
+    return q;
+}
+void inOrder(tree *l)
+{
+    if (!l)
+        return;
+    inOrder(l->left);
+    cout << l->value << ' ';
+    inOrder(l->right);
+}
+int main()
+{
+    tree *l = nullptr;
+    int a[] = {10, 9, 8, 1, 20, 19, 18, 17, 16};
+    for (int i : a)
+    {
+        insert(l, i);
+    }
+    inOrder(l);
+    cout << endl;
+    int k;
+    cout << "Enter the value of k = ";
+    cin >> k;
+    try
+    {
+        cout <<"Result = " << minimum(l, k) << '\n';
+    }
+    catch (const char *s)
+    {
+        cerr << "Number of nodes in the tree is lesser than query\n";
+    }
+}
